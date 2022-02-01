f5af88fce84fb6db6daf18bf2a3be846ce512547
Frederic Fox <dev.frederic.fox@gmail.com>
2022-02-01 12:14:54 +0100

    Add language

---
+++ components/TheHeader.vue
@@ -0,0 +1,16 @@
+<script setup lang="ts">
+const route = useRoute();
+const language = route.params.lang
+</script>
+
+<template>
+<div>
+<div id="header">Current language based on route: {{language}} - I am the header, I am present on every page. Just my language should change. - Current language based on route: {{language}}</div>
+</div>
+</template>
+<style>
+#header {
+    background-color: orangered;
+    color: white;
+}
+</style>
